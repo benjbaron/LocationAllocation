298b28d853279780c92ffacb454b2b7d7b7a302a
Benjamin Baron <benjamin.baron@me.com>
2016-03-11 18:05:57 +0100

    Added colors

--- src/constants.h
+++ src/constants.h
@@ -14,5 +14,8 @@ const int GRID_SIZE = 2000;
 const QColor CELL_COLOR = Qt::red;
 const double CELL_OPACITY = 0.5;
 const QColor ORANGE = QColor(255, 127, 0);
+const QColor BLUE   = Qt::darkBlue;
+const QColor RED    = Qt::darkRed;
+const QColor BLACK  = Qt::black;
 
 #endif // CONSTANTS_H
