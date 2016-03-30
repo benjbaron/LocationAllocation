0c61f1514b5db568e3d38ca24d4d873dc1589472
Benjamin Baron <benjamin.baron@me.com>
2016-03-30 17:46:42 +0200

    Moved the weightedAllocationLayer to this layer

--- src/compute_allocation_layer.cpp
+++ src/compute_allocation_layer.cpp
@@ -1,48 +1,183 @@
 #include "compute_allocation_layer.h"
-#include "allocation_dialog.h"
-#include "weighted_allocation_layer.h"
+#include "constants.h"
 
 bool ComputeAllocationLayer::load(Loader* loader) {
-    return false;
+    loader->loadProgressChanged((qreal) 0.0, "");
+    int count = 0;
+    int size = _allocation.size();
+    for(auto it = _allocation.begin(); it != _allocation.end(); ++it) {
+        Geometry* g = it.key();
+        _points.insert(g->getCenter(), it.value());
+        qDebug() << "geom" << it.key()->toString() << it.value()->geom->toString();
+        loader->loadProgressChanged((qreal) ++count / (qreal) size, "");
+    }
+
+    loader->loadProgressChanged((qreal) 1.0, "Done");
+    return true;
 }
 
 QGraphicsItemGroup *ComputeAllocationLayer::draw() {
-    return nullptr;
-}
+    int radius = 20;
+    int demandRadius = 10;
+    int deletedCandiateRadius = 7;
+    _groupItem = new QGraphicsItemGroup();
+    _groupItem->setHandlesChildEvents(false);
+    QColor normalColor = Qt::blue;
+    QColor selectedColor = Qt::green;
 
-void ComputeAllocationLayer::addMenuBar() {
-    Layer::addMenuBar();
-}
-
-/*
- * Compute the location allocation
-*/
-void ComputeAllocationLayer::computeAllocation() {
-    qDebug() << "Compute location allocation";
-
-    AllocationDialog diag(_parent);
-    if (diag.exec() == QDialog::Rejected) {
-        return;
+    double maxWeight = 0.0;
+    for(auto it = _points.begin(); it != _points.end(); ++it) {
+        double weight = it.value()->weight;
+        if(weight > maxWeight) maxWeight = weight;
     }
 
-    QHash<Geometry*, Allocation*> allocation; // resulting allocation
-    AllocationParams params;
-    diag.allocationParams(&params);
-    loadWithProgressDialog(_computeAllocation, &ComputeAllocation::processAllocationMethod, &params, &allocation);
+    double maxBackend = 0.0;
+    for(auto it = _points.begin(); it != _points.end(); ++it) {
+        for(auto jt = it+1; jt != _points.end(); ++jt) {
+            Geometry* geom1 = it.value()->geom;
+            Geometry* geom2 = jt.value()->geom;
+            GeometryMatrixValue* val1;
+            _spatialStats->getValue(&val1, geom1, geom2);
+            GeometryMatrixValue* val2;
+            _spatialStats->getValue(&val2, geom2, geom1);
 
-    // print the resulting allocation
-    for(auto it = allocation.begin(); it != allocation.end(); ++it) {
-        qDebug() << "candidate" << it.key()->getCenter().x()
-        << "allocated" << it.value()->weight << "with" << it.value()->demands.size()
-        << "demands" << "deleted" << it.value()->deletedCandidates.size();
+            double w = 0.0;
+            if(val1 != nullptr) w += val1->avgScore;
+            if(val2 != nullptr) w += val2->avgScore;
+
+            if(w > maxBackend)
+                maxBackend = w;
+        }
     }
 
-    // create a new layer
-    QString layerName = "Location allocation";
-    WeightedAllocationLayer* layer = new WeightedAllocationLayer(_parent, layerName, allocation);
-    _allocationLayers.append(layer);
+    for(auto it = _points.begin(); it != _points.end(); ++it) {
+        QPointF p = it.key();
+        qreal size = radius + radius * it.value()->weight / maxWeight;
 
-    // load the layer
-    Loader loader;
-    _parent->createLayer(layerName, layer, &loader);
+        GraphicsPoint* item = new GraphicsPoint(p, (int) size, p);
+        connect(item, &GraphicsPoint::mousePressedEvent, [=](QPointF id, bool mod, bool doubleClicked) {
+            qDebug() << "Clicked on point" << id << mod << doubleClicked;
+
+            if(doubleClicked) {
+                // select all points
+                for(auto jt = _points.begin(); jt != _points.end(); ++jt) {
+                    QPointF pt = jt.key();
+                    if(!_pointsGroups.contains(pt)) {
+                        // add the points to the group
+                        QGraphicsItemGroup* group = new QGraphicsItemGroup();
+                        for(Geometry* rp : _points.value(pt)->demands.keys()) {
+                            QGraphicsEllipseItem* i = new CircleGraphics(new Circle(rp->getCenter(), demandRadius));
+                            i->setBrush(QBrush(Qt::black));
+                            i->setPen(Qt::NoPen);
+                            QGraphicsLineItem* line = new QGraphicsLineItem(pt.x(), -1*pt.y(), rp->getCenter().x(), -1*rp->getCenter().y());
+                            line->setPen(QPen(QBrush(Qt::black),2.0));
+                            group->addToGroup(i);
+                            group->addToGroup(line);
+                        }
+                        for(Geometry* rp : _points.value(pt)->deletedCandidates) {
+                            QGraphicsEllipseItem* i = new CircleGraphics(new Circle(rp->getCenter(), deletedCandiateRadius));
+                            i->setBrush(QBrush(ORANGE));
+                            i->setPen(Qt::NoPen);
+                            group->addToGroup(i);
+                        }
+                        _pointsGroups.insert(pt, group);
+                        addGraphicsItem(group);
+                    }
+                    _pointsGroups.value(pt)->setVisible(true);
+                    _selectedPoints.insert(pt);
+                    _pointsGraphics.value(pt)->setBrush(QBrush(selectedColor));
+                }
+                return;
+            }
+
+            bool wasSelected = false;
+            if(_selectedPoints.contains(id)) {
+                // remove teh point from the selected set
+                _pointsGroups.value(id)->setVisible(false);
+                _pointsGraphics.value(id)->setBrush(QBrush(normalColor));
+                _selectedPoints.remove(id);
+                wasSelected = true; // prevent to display it again
+            }
+
+            if(!mod) {
+                // remove all of the previously selected points
+                for (auto jt = _selectedPoints.begin(); jt != _selectedPoints.end();) {
+                    QPointF selectedPoint = *jt;
+                    _pointsGroups.value(selectedPoint)->setVisible(false);
+                    _pointsGraphics.value(selectedPoint)->setBrush(QBrush(normalColor));
+                    jt = _selectedPoints.erase(jt); // delete the instance in the QSet
+                }
+            }
+
+            if(!wasSelected) {
+                // display all points related to the selected point
+                if(!_pointsGroups.contains(id)) {
+                    // add the points to the group
+                    QGraphicsItemGroup* group = new QGraphicsItemGroup();
+                    for(Geometry* rp : _points.value(id)->demands.keys()) {
+                        QGraphicsEllipseItem* i = new CircleGraphics(new Circle(rp->getCenter(), demandRadius));
+                        i->setBrush(QBrush(Qt::black));
+                        i->setPen(Qt::NoPen);
+                        QGraphicsLineItem* line = new QGraphicsLineItem(id.x(), -1*id.y(), rp->getCenter().x(), -1*rp->getCenter().y());
+                        line->setPen(QPen(QBrush(Qt::black),2.0));
+                        group->addToGroup(i);
+                        group->addToGroup(line);
+                    }
+                    for(Geometry* rp : _points.value(id)->deletedCandidates) {
+                        QGraphicsEllipseItem* i = new CircleGraphics(new Circle(rp->getCenter(), deletedCandiateRadius));
+                        i->setBrush(QBrush(ORANGE));
+                        i->setPen(Qt::NoPen);
+                        group->addToGroup(i);
+                    }
+
+                    _pointsGroups.insert(id, group);
+                    addGraphicsItem(group);
+                }
+                _pointsGroups.value(id)->setVisible(true);
+                _selectedPoints.insert(id);
+                _pointsGraphics.value(id)->setBrush(QBrush(selectedColor));
+            }
+        });
+
+        item->setBrush(QBrush(normalColor));
+        item->setPen(Qt::NoPen);
+        _pointsGraphics.insert(it.key(), item);
+        addGraphicsItem(item);
+
+
+        // add the backend lines
+        for(auto jt = it+1; jt != _points.end(); ++jt) {
+            Geometry* geom1 = it.value()->geom;
+            Geometry* geom2 = jt.value()->geom;
+            GeometryMatrixValue* val1;
+            _spatialStats->getValue(&val1, geom1, geom2);
+            GeometryMatrixValue* val2;
+            _spatialStats->getValue(&val2, geom2, geom1);
+            double w = 0.0;
+            if(val1 != nullptr) w += val1->avgScore;
+            if(val2 != nullptr) w += val2->avgScore;
+
+            qDebug() << geom1->toString() << geom2->toString() << w << val1 << val2;
+
+            if(w == 0.0)
+                continue;
+
+            double width = 2.0 + 10.0*(w / maxBackend);
+
+            QPointF pt1 = jt.key();
+            QGraphicsLineItem* line = new QGraphicsLineItem(p.x(), -1*p.y(), pt1.x(), -1*pt1.y());
+            line->setPen(QPen(QBrush(Qt::red),width));
+
+            // add label
+            QString labelText = QString::number(w);
+            QGraphicsTextItem* label = new QGraphicsTextItem(labelText, line);
+            QPointF center = line->boundingRect().center();
+            label->setPos(center.x() - (label->boundingRect().width()  / 2.0),
+                          center.y() - (label->boundingRect().height() / 2.0));
+            label->setRotation(-line->line().angle());
+            addGraphicsItem(line);
+        }
+    }
+
+    return _groupItem;
 }
--- src/compute_allocation_layer.h
+++ src/compute_allocation_layer.h
@@ -5,28 +5,69 @@
 #ifndef LOCALL_COMPUTEALLOCATIONLAYER_H
 #define LOCALL_COMPUTEALLOCATIONLAYER_H
 
+#include <QGraphicsEllipseItem>
+#include <QGraphicsSceneMouseEvent>
+
+// forward class declarations
+class Loader;
+
+class GraphicsPoint: public QObject, public QGraphicsEllipseItem {
+    Q_OBJECT
+public:
+    GraphicsPoint():
+            QGraphicsEllipseItem(), _id(QPointF()) {}
+    GraphicsPoint(qreal x, qreal y, qreal w, qreal h, QPointF id):
+            QGraphicsEllipseItem(x, -1*y, w, -1*h), _id(id) {}
+    GraphicsPoint(QPointF p, int radius, QPointF id):
+            QGraphicsEllipseItem(p.x()-radius, -1*(p.y()+radius), 2*radius, (2*radius)), _id(id) {}
+
+    signals:
+    void mousePressedEvent(QPointF, bool, bool);
+
+protected:
+    void mousePressEvent(QGraphicsSceneMouseEvent *event) {
+        if(event->button() == Qt::LeftButton) {
+            event->accept();
+            emit mousePressedEvent(_id, (event->modifiers() == Qt::ShiftModifier), false);
+        } else {
+            event->ignore();
+        }
+    }
+    void mouseDoubleClickEvent(QGraphicsSceneMouseEvent *event) {
+        if(event->button() == Qt::LeftButton) {
+            event->accept();
+            emit mousePressedEvent(_id, (event->modifiers() == Qt::ShiftModifier), true);
+        } else {
+            event->ignore();
+        }
+    }
+
+private:
+    QPointF _id;
+};
 
 #include "layer.h"
-
-// forward class declaration
-class WeightedAllocationLayer;
+#include "spatial_stats.h"
 
 class ComputeAllocationLayer : public Layer {
     Q_OBJECT
 public:
-    ComputeAllocationLayer(MainWindow* parent = 0, QString name = 0, ComputeAllocation* computeAllocation = nullptr):
-            Layer(parent, name), _computeAllocation(computeAllocation) {}
+    ComputeAllocationLayer(MainWindow* parent = 0,
+                           QString name = 0,
+                           const QHash<Geometry*,Allocation*>& alloc = QHash<Geometry*,Allocation*>(),
+                           SpatialStats* spatialStats = nullptr):
+            Layer(parent, name), _allocation(alloc), _spatialStats(spatialStats) {}
 
     virtual bool load(Loader *loader);
     virtual QGraphicsItemGroup* draw();
-    virtual void addMenuBar();
-
-public slots:
-    void computeAllocation();
 
 private:
-    ComputeAllocation* _computeAllocation;
-    QList<WeightedAllocationLayer*> _allocationLayers;
+    const QHash<Geometry*,Allocation*>& _allocation;
+    QHash<QPointF, Allocation*> _points;
+    QHash<QPointF, GraphicsPoint*> _pointsGraphics;
+    QHash<QPointF, QGraphicsItemGroup*> _pointsGroups;
+    QSet<QPointF> _selectedPoints;
+    SpatialStats* _spatialStats;
 };
 
 
